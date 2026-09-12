c16d3591c04e976b446f5973cfa53e18d7b862df
BarrieMosque <barriemosque@gmail.com>
2026-09-12 03:47:58 -0400

    Update school email addresses in MAILS enum

--- Frontend/src/components/Shared/enums.js
+++ Frontend/src/components/Shared/enums.js
@@ -2,7 +2,7 @@ export const MAILS = {
     imam: "imam@barriemosque.ca",
     general: ["barriemosque@gmail.com", "info@barriemosque.ca"],
     secretary: "organizingsecretary@barriemosque.ca",
-    school: "school@barriemosque.ca",
+    school: "barriemosque@gmail.com", "abid.siddiqui141@gmail.com",
     arabicSchool: "barriearabicschool@gmail.com",
     revert: "info@barriemosque.ca"
 }
