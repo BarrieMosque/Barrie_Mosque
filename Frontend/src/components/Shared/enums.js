2e994e6fe5f281f2f3a33aeb01f44090d92bc5db
BarrieMosque <barriemosque@gmail.com>
2026-09-14 18:20:10 -0400

    Update enums.js

--- Frontend/src/components/Shared/enums.js
+++ Frontend/src/components/Shared/enums.js
@@ -1,8 +1,8 @@
 export const MAILS = {
-    imam: "imam@barriemosque.ca",
+    imam: ["barriemosque@gmail.com","imam@barriemosque.ca"],
     general: ["barriemosque@gmail.com", "info@barriemosque.ca"],
-    secretary: "organizingsecretary@barriemosque.ca",
+    secretary: ["barriemosque@gmail.com","organizingsecretary@barriemosque.ca"],
     school: ["barriemosque@gmail.com", "abid.siddiqui141@gmail.com"],
     arabicSchool: "barriearabicschool@gmail.com",
-    revert: "info@barriemosque.ca"
+    revert: ["barriemosque@gmail.com","info@barriemosque.ca"],
 }
