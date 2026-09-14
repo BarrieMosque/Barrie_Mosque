0cad19b89d09735b3a77964c7823bbb50cfff5fb
BarrieMosque <barriemosque@gmail.com>
2026-09-14 18:21:59 -0400

    Add additional email addresses to MAILS enum

--- Frontend/src/components/Shared/enums.js
+++ Frontend/src/components/Shared/enums.js
@@ -1,8 +1,8 @@
 export const MAILS = {
-    imam: ["barriemosque@gmail.com","imam@barriemosque.ca"],
+    imam: ["barriemosque@gmail.com","imam@barriemosque.ca","imambarriemosque@gmail.com"],
     general: ["barriemosque@gmail.com", "info@barriemosque.ca"],
     secretary: ["barriemosque@gmail.com","organizingsecretary@barriemosque.ca"],
     school: ["barriemosque@gmail.com", "abid.siddiqui141@gmail.com"],
     arabicSchool: "barriearabicschool@gmail.com",
-    revert: ["barriemosque@gmail.com","info@barriemosque.ca"],
+    revert: ["barriemosque@gmail.com","info@barriemosque.ca","s5khan@yahoo.com"],
 }
